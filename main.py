deb1c6c9336b609a20f97527fddf8c328cd6cfef
Marek Janda <Marek.Janda@jehall.co.uk>
2021-12-23 15:36:45 +0000

    CoolProp cycle adapted to refrigerant as variable

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from helpers import bar_to_kPa, kw_to_tr
 
 deg = u"\N{DEGREE SIGN}"
 
-refrigerant = 'R32'
+refrigerant = 'R134a'
 
 '''Suction Conditions'''
 t_sat_evap = 1.7 #degC
@@ -43,8 +43,8 @@ s2 = s1 # [kJ/kg]
 h1 = PropsSI('H', 'T', t_sat_evap+superheat+273.15, 'P', p_suc*100000, refrigerant)/1000 # [kJ/kg]
 h2 = PropsSI('H', 'P', p_dis*100000, 'S', s1*1000, refrigerant)/1000 # [kJ/kg]
 h2a = ((h2-h1)/ie)+h1 # [kJ/kg]
-T_dis_ie = PropsSI('T','P', p_dis*100000, 'S', s2, refrigerant) # Isentropic Discharge Temperature
-T_dis_a = PropsSI('T','P', p_dis*100000, 'H', h2a, refrigerant) # Actual Discharge Temperature
+#T_dis_ie = PropsSI('T','P', p_dis*100000, 'S', s2, refrigerant) # Isentropic Discharge Temperature
+#T_dis_a = PropsSI('T','P', p_dis*100000, 'H', h2a, refrigerant) # Actual Discharge Temperature
 h3 = PropsSI('H', 'T',t_sat_cond-subcooling+273.15, 'P', p_dis*100000, refrigerant)/1000 # [kJ/kg]
 h4 = h3 # [kJ/kg]
 qL = h1 - h4 #/ Latent heat [kj/Kg]
@@ -81,8 +81,6 @@ print(h2)
 print(h2a)
 print(s1)
 print(s2)
-print(T_dis_ie)
-print(T_dis_a)
 print(h3)
 print(h4)
 print(qL)
@@ -102,9 +100,9 @@ h_points = np.array([h1, h2a, h3, h4, h1])
 p_points = np.array([p_suc, p_dis, p_dis, p_suc, p_suc])
 
 
-pp = PropertyPlot('HEOS::R32', 'PH', unit_system='EUR')
+pp = PropertyPlot(f"HEOS::{refrigerant}", 'PH', unit_system='EUR')
 pp.calc_isolines()
-cycle = SimpleCompressionCycle('HEOS::R32', 'PH', unit_system='EUR')
+cycle = SimpleCompressionCycle(f"HEOS::{refrigerant}", 'PH', unit_system='EUR')
 T0 = 273.15+t_sat_evap
 pp.state.update(CoolProp.QT_INPUTS,0.0,T0-superheat)
 p0 = pp.state.keyed_output(CoolProp.iP)
@@ -131,7 +129,7 @@ plt.grid()
 plt.savefig("Charts/Cycle.png")
 
 pp.draw_process(sc)
-pp.title("p-h Diagram")
+pp.title(f"p-h Diagram of {refrigerant}")
 pp.savefig('Charts/p-h.png')
 #pp.show()
 
